eeb58ef4774bde992fbc422327759ddd85714554
Rohan Banerjee <banerjeerohan87@gmail.com>
2021-11-12 21:39:45 +0530

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDetailsComponent } from './todo-details/todo-details.component';
+import { TodoservService } from './todoserv.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent,
+    TodoDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [TodoservService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
